293a92dbe9152ea2b2768f24019545633bddf24f
Martin Jaskulla <martin.jaskulla@trustedshops.com>
2024-07-18 12:25:06 +0200

    npm run build && npm publish

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      formats: ["es"],
+      entry: path.resolve(__dirname, "src/main.jsx"),
+      fileName: "main",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  }
 })
